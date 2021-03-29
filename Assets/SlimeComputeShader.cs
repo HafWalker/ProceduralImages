057ed0d075cfa9fd4c0d67a4227fb6c297a4582d
HafWalker <hernan.angel.ferrari@gmail.com>
2021-03-29 18:28:54 -0300

    Added base SlimeComputeShader

---
+++ Assets/SlimeComputeShader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeComputeShader : MonoBehaviour
+{
+    public ComputeShader computeShader;
+    public RenderTexture outRenderTextrure;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (outRenderTextrure == null) {
+            outRenderTextrure = new RenderTexture(256,256,24);
+            outRenderTextrure.enableRandomWrite = true;
+            outRenderTextrure.Create();
+        }
+
+        computeShader.SetTexture(0, "Result", outRenderTextrure);
+        computeShader.Dispatch(0, 256 / 8, 256 / 8, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
